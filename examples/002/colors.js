9008bb4c31727ef07ff19dae9d867a1b5375b261
Shaun Michael K. Stone <shaunmstone@gmail.com>
2016-11-28 10:20:48 +0000

    Update colors.js

--- examples/002/colors.js
+++ examples/002/colors.js
@@ -8,3 +8,43 @@ require('colors');
 console.log('This is a success message'.green);
 console.log('This is a warning message'.yellow);
 console.log('This is a error message'.red);
+
+// Log output with color
+log(message, type) => {
+	let colorMessage;
+	switch (type) {
+		case 'error':
+			colorMessage = message.red;
+			break;
+		case 'warning':
+			colorMessage = message.yellow;
+			break;
+		case 'success':
+			colorMessage = message.green;
+			break;
+		default:
+			colorMessage = message;
+	}
+	console.log(colorMessage);
+}
+
+// Log output with prefix
+const log = (message, type) => {
+	let colorMessage;
+	switch (type) {
+		case 'error':
+			colorMessage = '[ERROR] ' + message.red;
+			break;
+		case 'warning':
+			colorMessage = '[WARNING] ' + message.yellow;
+			break;
+		case 'success':
+			colorMessage = '[SUCCESS] ' + message.green;
+			break;
+		default:
+			colorMessage = '[INFO] ' + message;
+	}
+	console.log(colorMessage);
+}
+
+modules.exports = log;
