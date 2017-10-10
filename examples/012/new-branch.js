f739793c872b3e7428cc7b929650adc2819949b6
Shaun Stone <shaunmstone@gmail.com>
2017-10-10 20:13:37 +0100

    latest changes

---
+++ examples/012/new-branch.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+/*jshint esversion: 6, node: true */
+"use strict";
+
+require('colors');
+const shell = require('shelljs');
+const readLineSync = require('readline-sync');
+
+// Changing directory
+shell.cd('nobo-delivery');
+
+// making sure we have the latest changes
+shell.exec('git pull origin master');
+
+// prompt for the ticket number
+const ticketNumber = readLineSync.question('What is the ticket ID? ', {
+    limit: /^(?=\s*\S).*$/,
+    limitMessage: 'Please enter a ticket number (GOT-123)'
+});
+
+// Create a new branch
+shell.exec(`git checkout -b ${ticketNumber}`);
+
+// Let the user know it was created
+console.log(`Changed to new branch ${ticketNumber}`);
---
+++ examples/012/setup.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+
+/*jshint esversion: 6, node: true */
+"use strict";
+
+require('colors');
+const shell = require('shelljs');
+
+const repository = 'https://github.com/smks/nobo-delivery';
+
+console.log(`Cloning ${repository}`.cyan);
+shell.exec(`git clone ${repository} --progress -b master`);
