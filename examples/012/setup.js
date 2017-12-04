47298e459e58bd72ce120958d9ed210d146d0144
Shaun Stone <shaunmstone@gmail.com>
2017-12-04 22:39:28 +0000

    removed nobot delivery in root

--- examples/012/setup.js
+++ examples/012/setup.js
@@ -7,4 +7,7 @@ const { repositories } = require('./config');
 const { delivery } = repositories;
 
 console.log(`Cloning ${delivery}`.cyan);
+
+shell.cd(__dirname);
+
 shell.exec(`git clone ${delivery} --progress -b master`);
