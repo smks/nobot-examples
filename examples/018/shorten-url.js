d6b50320c7a0f6e84904e55121ffef780ed1d8f9
Shaun Stone <shaunmstone@gmail.com>
2017-12-06 10:59:22 +0000

    added node ltd * stable

--- examples/018/shorten-url.js
+++ examples/018/shorten-url.js
@@ -5,18 +5,30 @@
  */
 
 const Bitly = require('bitly');
-const config = require('./config.json');
+const { BITLY_TOKEN } = require('./config');
 
-const bitly = new Bitly(config.BITLY_TOKEN);
+const STATUS_CODE_OK = 200;
+const bitly = new Bitly(BITLY_TOKEN);
 
 const args = process.argv.slice(2);
 const [urlToShorten] = args;
 
-if (urlToShorten.match('http') === null) {
-  console.log("Please pass a string as a URL e.g. 'http://www.opencanvas.co.uk'");
+const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
+const regex = new RegExp(expression);
+
+if (urlToShorten === undefined || urlToShorten.match(regex) === null) {
+  console.log("Please pass a string in URL form e.g. 'http://www.opencanvas.co.uk'");
   process.exit(0);
 }
 
 bitly.shorten(urlToShorten)
-  .then(response => console.log(`Shortened URL is: ${response.data.url}`))
+  .then(response => {
+    const statusCode = response.status_code;
+    const statusText = response.status_txt;
+    if (statusCode !== STATUS_CODE_OK) {
+      console.error('Something went wrong:', statusText);
+    } else {
+      console.log(`Shortened URL is: ${response.data.url}`);
+    }
+  })
   .catch(error => console.log(error));
