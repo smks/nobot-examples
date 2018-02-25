cc9aaba99204d08f84fd4fa4487f04ee7eff8b11
smks <shaunmstone@gmail.com>
2018-02-25 09:44:35 +0000

    changed to 28

--- examples/003/parse-querystring.js
+++ examples/003/parse-querystring.js
@@ -2,7 +2,7 @@
 
 const querystring = require('querystring');
 
-const url = 'http://www.opencanvas.co.uk?myName=Shaun&myAge=27&comment=Yes+I+am+getting+old';
+const url = 'http://www.opencanvas.co.uk?myName=Shaun&myAge=28&comment=Yes+I+am+getting+old';
 const parsedUrl = querystring.parse(url.substring(url.indexOf('?') + 1));
 
 console.log(`Hi my name is ${parsedUrl.myName}`);
