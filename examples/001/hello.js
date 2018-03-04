d9c642cdd7dc1c077bd9f53b7e582033cdfc8789
Shaun Stone <shaunmstone@gmail.com>
2018-03-04 14:40:55 +0000

    Update hello.js

--- examples/001/hello.js
+++ examples/001/hello.js
@@ -4,8 +4,8 @@ const args = process.argv.slice(2);
 const [name] = args;
 
 if (name === undefined) {
-  console.log('Please pass a name: node hello.js \'Shaun\'');
+  console.log('Please pass a name, e.g. node hello.js Shaun');
   process.exit(0);
 }
 
-console.log(`Good day to you ${name}`);
+console.log(`Good day to you, ${name}`);
