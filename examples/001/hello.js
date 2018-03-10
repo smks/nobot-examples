6e441ce712ea430d502c97e365f9549d4e1d3709
Shaun Stone <shaunmstone@gmail.com>
2018-03-10 12:57:32 +0000

    Update hello.js

--- examples/001/hello.js
+++ examples/001/hello.js
@@ -3,7 +3,7 @@ const [name] = args;
 
 if (name === undefined) {
   console.log('Please pass a name, e.g. node hello.js Shaun');
-  process.exit(0);
+  process.exit(1); // 1 indicates failure
 }
 
 console.log(`Good day to you, ${name}`);
