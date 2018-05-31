206a3a85e47dd58154863130ce3d14dd22da3075
Shaun Stone <shaunmstone@gmail.com>
2018-05-31 19:53:51 +0100

    constant json whitespace

--- examples/007/helpers/write-json.js
+++ examples/007/helpers/write-json.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 
+const JSON_WHITESPACE = 4;
+
 const writeJson = (file, contents) => new Promise((resolve, reject) => {
-  fs.writeFile(file, JSON.stringify(contents, null, 4), (err) => {
+  fs.writeFile(file, JSON.stringify(contents, null, JSON_WHITESPACE), (err) => {
     if (err) {
       reject(err);
     }
