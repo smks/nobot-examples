a42510a79f9e74995d4246397c8d7dfe9f8a26f0
wavesailor <wavesailor@yahoo.com>
2018-10-16 12:44:50 -0400

    Update open.js
    Added check for MAC_PLATFORM and code to open FireFox in Linux

--- examples/006/open.js
+++ examples/006/open.js
@@ -2,6 +2,7 @@ const { platform } = require('os');
 const { exec } = require('child_process');
 
 const WINDOWS_PLATFORM = 'win32';
+const MAC_PLATFORM = 'darwin';
 
 const osPlatform = platform();
 const args = process.argv.slice(2);
@@ -16,8 +17,10 @@ let command;
 
 if (osPlatform === WINDOWS_PLATFORM) {
   command = `start microsoft-edge:${url}`;
-} else {
+} else if (osPlatform === MAC_PLATFORM) {
   command = `open -a "Google Chrome" ${url}`;
+} else {
+  command = `firefox ${url}`;
 }
 
 console.log(`executing command: ${command}`);
