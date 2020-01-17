14b8778a44d5bf433978ad1d3c4ac95fe9bbd581
Trevor Specht <trevor.specht@mapbox.com>
2020-01-17 09:40:20 -0500

    added process.uptime

--- examples/002/process.js
+++ examples/002/process.js
@@ -6,6 +6,7 @@ process.stdin.on('readable', () => {
     const chunk = process.stdin.read();
 	if (chunk !== null) {
         process.stdout.write(`You entered ${chunk}`);
+        process.stdout.write(`This script has been running for ${process.uptime()} seconds. \n`);
         process.exit(0);
     }
 });
