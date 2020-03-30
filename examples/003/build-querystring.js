08e20dc6ab208271e637310c679186e725db3b1c
Trevor Specht <trevor.specht@mapbox.com>
2020-03-29 23:28:59 -0400

    004

--- examples/003/build-querystring.js
+++ examples/003/build-querystring.js
@@ -1,1 +1,16 @@
+// querystring is a Node.js standard library module that takes an object
+// of key-value pairs and using the stringify method returns a formatted query string
+
 const querystring = require('querystring');
+
+const apiHost = 'https://jira.my-company.com/rest/api/latest/search?jql=';
+
+const jqlParams = {
+    assignee: 'trevor.specht',
+    startAt: 2,
+    maxResults: 2
+};
+
+const apiUrl = `${apiHost}"${querystring.stringify(jqlParams)}"`;
+
+console.log(`API call: ${apiUrl}`);
--- examples/003/parse-querystring.js
+++ examples/003/parse-querystring.js
@@ -1,1 +1,11 @@
+// querystring.parse will take a query string and create an object
+
 const querystring = require('querystring');
+
+const url = 'https://jira.my-company.com/rest/api/latest/search?jql="assignee=trevor.specht&startAt=2&maxResults=2"';
+
+const parsedUrl = querystring.parse(url.substring(url.indexOf('?') + 1));
+
+console.log(`assignee: ${parsedUrl.assignee}`);
+console.log(`start at: ${parsedUrl.startAt}`);
+console.log(`max results: ${parsedUrl.maxResults}`);
