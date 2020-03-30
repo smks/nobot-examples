a7f8be24ef6e083883f2f5cff1055656f6af7037
Trevor Specht <trevor.specht@mapbox.com>
2020-03-29 23:07:06 -0400

    add process.on exit listener

--- examples/002/process.js
+++ examples/002/process.js
@@ -6,7 +6,10 @@ process.stdin.on('readable', () => {
     const chunk = process.stdin.read();
 	if (chunk !== null) {
         process.stdout.write(`You entered ${chunk}`);
-        process.stdout.write(`This script has been running for ${process.uptime()} seconds. \n`);
-        process.exit(0);
+        process.stdout.write(`Process ${process.pid} has been running for ${process.uptime()} seconds. \n`);
+        process.on('exit', (code) => {
+            console.log(`Exiting process now with code ${code}`);
+            process.exit(code);
+        })
     }
 });
