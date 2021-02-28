8a02804e06f18bdc7e28e817710e7cfdc2f7d69b
Kessyus Fófano <kessyus@gmail.com>
2021-02-28 07:59:30 -0300

    6 chapters done

---
+++ examples/006/myOpen.js
@@ -0,0 +1,29 @@
+const { platform } = require("os");
+const { exec } = require("child_process");
+
+const WINDOWS_PLATFORM = "win32";
+
+const osPlatform = platform();
+
+const args = process.argv.slice(2);
+
+const [url] = args;
+
+if (url === undefined) {
+  console.error(
+    'Por favor informe uma URL, i.e. "http://www.opencanvas.co.uk"'
+  );
+  process.exit(0);
+}
+
+let command;
+
+if (osPlatform === WINDOWS_PLATFORM) {
+  command = `start microsoft-edge:${url}`;
+} else {
+  command = `open -a "Google Chrome" ${url}`;
+}
+
+console.log(`executing command: ${command}`);
+
+exec(command);
