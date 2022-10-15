5b174bf624a63c37a0307cf7bfca0455f67ddadd
ariepaulus <averburgh@gmail.com>
2022-10-15 07:38:37 +0200

    Update os.js

--- examples/005/os.js
+++ examples/005/os.js
@@ -1,13 +1,20 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* The os module allows us to query hardware specifications of our computer */
 const os = require('os');
 
 const homeDirectory = os.homedir();
+
 console.log(`Your home directory is: ${homeDirectory}`);
 
+/* You can identify your OS platform with the os.platform function */
 const osPlatform = os.platform();
-console.log(`The OS platform is: ${osPlatform}`);
 
+console.log(`Your operating system platform is: ${osPlatform}`);
+
+/* Identify CPU specs with the os.cpus function */
 const cpuCores = os.cpus();
 const coreCount = cpuCores.length;
 const cpuModel = cpuCores[0].model;
 
-console.log(`I can see your ${cpuModel} has ${coreCount} cores.`);
+console.log(`Your ${cpuModel} has ${coreCount} cores.`);
